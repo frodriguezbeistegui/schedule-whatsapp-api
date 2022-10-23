823f63ee6debcb70764362b81a2f462e0079151b
facundo <frodriguezbeistegui@gmail.com>
2022-10-23 12:26:50 -0300

    fix

--- scripts/example.js
+++ scripts/example.js
@@ -26,7 +26,7 @@ const sendMessage = (message, phoneNumber) => {
 const singleMessage = ({message, phoneNumber, cronConf}) => {
   const job = schedule.scheduleJob(`${cronConf.second ? cronConf.second + ' ' : ''}${cronConf.minute} ${cronConf.hours} ${cronConf.dayOfMoth} ${cronConf.month} ${cronConf.weekDay}`,
   ()=>{ 
-    this.sendMessage(message, phoneNumber)
+    sendMessage(message, phoneNumber)
   })
 }
 
@@ -36,7 +36,7 @@ const multipleMessages =  ({messagesArr, phoneNumber, cronConf}) => {
   const job = schedule.scheduleJob(`${cronConf.second ? cronConf.second + ' ' : null}${cronConf.minute} ${cronConf.hours} ${cronConf.dayOfMoth} ${cronConf.month} ${cronConf.weekDay}`,
     ()=>{  
       for (let message of messagesArr){
-        setTimeout(()=>{ this.sendMessage(message, phoneNumber)}, 1000)
+        setTimeout(()=>{ sendMessage(message, phoneNumber)}, 1000)
       }
     })
 }
@@ -45,7 +45,7 @@ const multipleMessages =  ({messagesArr, phoneNumber, cronConf}) => {
 const randomInArray = ({messagesArr, phoneNumber, cronConf}) => {
   const job = schedule.scheduleJob(`${cronConf.second ? cronConf.second + ' ' : null}${cronConf.minute} ${cronConf.hours} ${cronConf.dayOfMoth} ${cronConf.month} ${cronConf.weekDay}`, ()=>{ 
   const randomMessage = messagesArr[Math.floor((Math.random()* messagesArr.length))]
-  this.sendMessage(randomMessage, phoneNumber)
+  sendMessage(randomMessage, phoneNumber)
   })
 }
 
@@ -53,19 +53,19 @@ const randomInArray = ({messagesArr, phoneNumber, cronConf}) => {
 
   // EXECUTIONS
 
-singleMessage({
-  message: 'Test message written earlier',
-  phoneNumber: 11111111, 
-  cronConf: {
-    // useful resource: https://crontab.guru/
-    hours: '12',
-    minute: '0',
-    second: '0',
-    dayOfMoth: '23',
-    month: '10',
-    weekDay: '*',
-  }
-})
+// singleMessage({
+//   message: 'Que onda martin',
+//   phoneNumber: 3517452200, 
+//   cronConf: {
+//     // useful resource: https://crontab.guru/
+//     hours: '*',
+//     minute: '*',
+//     second: '0,5,10,15,20,25,30,35,40,45,50,55',
+//     dayOfMoth: '*',
+//     month: '*',
+//     weekDay: '*',
+//   }
+// })
 
 // sendRandomInArray( {
 // messagesArr: ['random message 1', 'random message 2', 'random message 3', 'random message 4', 'random message 5', 'random message 6'],
@@ -80,15 +80,14 @@ singleMessage({
 //   weekDay: '*',
 // }}
 // )
-
-// sendMultipleMessages({messagesArr: ['test message 1', 'test message 2', 'test message 3', 'test message 4', 'test message 5', 'test message 6'],
-// phoneNumber: 1111111111,
-// cronConf: {
-//   // useful resource: https://crontab.guru/
-//   second: '0,10,20,30,40,50', // optional
-//   minute: '*',
-//   hours: '*',
-//   dayOfMoth: '*',
-//   month: '*',
-//   weekDay: '*',
-// }})
+multipleMessages({messagesArr: ['test message 1', 'test message 2', 'test message 3', 'test message 4', 'test message 5', 'test message 6'],
+phoneNumber: 3517452200,
+cronConf: {
+  // useful resource: https://crontab.guru/
+  second: '0,5,10,15,20,25,30,35,40,45,50,55', // optional
+  minute: '*',
+  hours: '*',
+  dayOfMoth: '*',
+  month: '*',
+  weekDay: '*',
+}})
