aa8475d3a959ac66e99f7f87d81f338e23db22fa
facundo <frodriguezbeistegui@gmail.com>
2022-10-23 01:17:58 -0300

    new features: multiple messages and random message within array in scripts/example.js

--- scripts/example.js
+++ scripts/example.js
@@ -2,37 +2,84 @@ const axios = require('axios');
 const schedule = require('node-schedule');
 
 // this function sends messages to argentinian phone numbers
-const sendMessages = (message, phoneNumber) => {
+const sendMessage = (message, phoneNumber) => {
     axios.post(`http://localhost:5000/chat/sendmessage/549${phoneNumber}`, {
       message
     })
-      .then(function (response) {
-        console.log();
-        console.log(response);
-      })
+      .then(function () {
+        console.log(`new message: ${message} \n sent to ${phoneNumber}`);
+        })
       .catch(function (error) {
         console.log(error);
       });
 
 }
 
-const example = () => {
-    // useful resource: https://crontab.guru/
-    const second = '0,5,10,15,20,25,30,35,40,45,50,55' // optional
-    const minute = '*'
-    const hours = '*'
-    const dayOfMoth = '*'
-    const month = '*'
-    const weekDay = '*'
-    
-    const messages = ['test message 1', 'test message 2', 'test message 3', 'test message 4', 'test message 5', 'test message 6']
-    // envia un mensaje cada 5 segundos (es simil cron, pero no es cron porque permite agregar segundos) 
-    const job = schedule.scheduleJob(`${second ? second + ' ' : null}${minute} ${hours} ${dayOfMoth} ${month} ${weekDay}`, ()=>{ 
-        // ingresa aun index random del array para seleccionar un mesanje diferente cada vez que se ejecuta
-        const randomMessage = messages[Math.floor((Math.random()* messages.length))]
-        //envia un mensaje a un numero de telefono
-        sendMessages(randomMessage, 3585268935)
-    })
+const sendSingleMessage = ({message, phoneNumber, cronConf}) => {
+  const job = schedule.scheduleJob(`${cronConf.second ? cronConf.second + ' ' : null}${cronConf.minute} ${cronConf.hours} ${cronConf.dayOfMoth} ${cronConf.month} ${cronConf.weekDay}`, ()=>{ 
+    //envia un mensaje a un numero de telefono
+    sendMessage(message, phoneNumber)
+  })
 }
 
-example()
+const sendMultipleMessages =  ({messagesArr, phoneNumber, cronConf}) => {
+  const job = schedule.scheduleJob(`${cronConf.second ? cronConf.second + ' ' : null}${cronConf.minute} ${cronConf.hours} ${cronConf.dayOfMoth} ${cronConf.month} ${cronConf.weekDay}`, ()=>{ 
+    //envia un mensaje a un numero de telefono
+    for (let message of messagesArr){
+     setTimeout(()=>{ sendMessage(message, phoneNumber)}, 1000)
+    }
+  })
+
+
+
+}
+
+const sendRandomInArray = ({messagesArr, phoneNumber, cronConf}) => {
+  const job = schedule.scheduleJob(`${cronConf.second ? cronConf.second + ' ' : null}${cronConf.minute} ${cronConf.hours} ${cronConf.dayOfMoth} ${cronConf.month} ${cronConf.weekDay}`, ()=>{ 
+    // ingresa aun index random del array para seleccionar un mesanje diferente cada vez que se ejecuta
+    const randomMessage = messagesArr[Math.floor((Math.random()* messages.length))]
+    //envia un mensaje a un numero de telefono
+    sendMessage(randomMessage, phoneNumber)
+  })
+}
+
+
+// sendSingleMessage({
+// message: 'Hola mundo',
+//  phoneNumber: 11111111111, 
+// cronConf: {
+//   // useful resource: https://crontab.guru/
+//   second: ',10,15,20,25,30,35,40,45,50,55', // optional
+//   minute: '*',
+//   hours: '*',
+//   dayOfMoth: '*',
+//   month: '*',
+//   weekDay: '*',
+// }
+// })
+
+// sendRandomInArray( {
+// messagesArr: ['test message 1', 'test message 2', 'test message 3', 'test message 4', 'test message 5', 'test message 6'],
+//  phoneNumber: 111111111,
+// cronConf: {
+//   // useful resource: https://crontab.guru/
+//   second: '0,5,10,15,20,25,30,35,40,45,50,55', // optional
+//   minute: '*',
+//   hours: '*',
+//   dayOfMoth: '*',
+//   month: '*',
+//   weekDay: '*',
+// }}
+// )
+
+// sendMultipleMessages({messagesArr: ['hola', 'chofi', 'como', 'estas?', 'esto es una sola ejecucion'],
+// phoneNumber: 1111111111,
+// cronConf: {
+//   // useful resource: https://crontab.guru/
+//   second: '0,10,20,30,40,50', // optional
+//   minute: '*',
+//   hours: '*',
+//   dayOfMoth: '*',
+//   month: '*',
+//   weekDay: '*',
+// }})
